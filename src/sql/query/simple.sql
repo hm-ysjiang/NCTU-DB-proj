f11d28d88321d4ac4f12bb030540ee0236e2db22
weii41392 <weii41392@gmail.com>
2020-07-10 12:54:18 +0800

    Simple Search

--- src/sql/query/simple.sql
+++ src/sql/query/simple.sql
@@ -1,13 +1,13 @@
 -- 簡易查詢 : 職缺名稱/公司名稱 + 地區
--- 假設 $NAME 和 $CC 和 $TD 是 input
 
--- Output 除了 jobinfo 還需要有什麼?
-
+-- log
 -- 2020/7/10 : 幹不知道為啥出來是empty set
+-- 2020/7/10 : 輸入特定關鍵字(@name)和地區(@cc, @td)的功能可以run了。Output 除了 jobinfo 和 com_name 以外還需要有什麼?
 
-SET @name := '', @cc := '臺中市', @td := NULL;
+SET @name = 'nike', @cc = NULL, @td = NULL;
 
-SELECT  jobinfo.job_name AS JOB_NAME,
+SELECT  company.com_name AS COM_NAME,
+        jobinfo.job_name AS JOB_NAME,
         jobinfo.degree AS DEGREE,
         jobinfo.low_salary AS LOW_SALARY,
         jobinfo.high_salary AS HIGH_SALARY,
@@ -17,15 +17,14 @@ SELECT  jobinfo.job_name AS JOB_NAME,
         jobinfo.is_night AS IS_NIGHT,
         jobinfo.needed_num AS NEEDED_NUM
 
-FROM    jobinfo, company,
-                        (SELECT  job.job_id
-                        FROM    job, localarea
-                                
-
-                        WHERE       job.area_id = localarea.area_id
-                                AND ((@cc = NULL) OR (@cc = localarea.area_cc_name))
-                                AND ((@td = NULL) OR (@td = localarea.area_td_name))
-                    ) AS job
-WHERE   jobinfo.job_id=job.job_id
-        AND ((jobinfo.job_name LIKE "%@name%") OR (company.com_name LIKE "%@name%"))
+FROM    jobinfo, company, (
+                                SELECT  job.job_id, job.com_id
+                                FROM    job, localarea
+                                WHERE       job.area_id = localarea.area_id
+                                        AND (IFNULL(@cc, 1) OR (@cc = localarea.area_cc_name)) -- IFNULL(var, value) : if var is null then return value, else return var
+                                        AND (IFNULL(@td, 1) OR (@td = localarea.area_td_name)) -- 不能寫 var=NULL (expression)
+                                ) AS job
+WHERE   jobinfo.job_id = job.job_id
+        AND job.com_id = company.com_id
+        AND ((jobinfo.job_name LIKE CONCAT('%', @name, '%')) OR (company.com_name LIKE CONCAT('%', @name, '%'))) -- 記得用CONCAT連結字串不要亂用operator
 ;
