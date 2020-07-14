381de101f988ccd5281bc7add8b59a3f3c8d96a8
hm-ysjiang <hmysjiang.git@gmail.com>
2020-07-14 17:05:41 +0800

    Job name query can be empty

--- src/web/frontend/script/advance.js
+++ src/web/frontend/script/advance.js
@@ -49,101 +49,96 @@ ws.onmessage = evt => {
 
 runSearch = () => {
     search = document.getElementById('search-input').value
-    if (search) {
-        document.getElementById('search-input').className = 'form-control form-control-lg'
-        iden = Math.random().toString(36).substr(2)
+    document.getElementById('search-input').className = 'form-control form-control-lg'
+    iden = Math.random().toString(36).substr(2)
 
-        loader = document.getElementById('loader')
-        resBlock = document.getElementById('search-res')
-        resContainer = document.getElementById('search-res-container')
+    loader = document.getElementById('loader')
+    resBlock = document.getElementById('search-res')
+    resContainer = document.getElementById('search-res-container')
 
-        pos_field = document.getElementById('pos-field').value
+    pos_field = document.getElementById('pos-field').value
+    pos_name = ''
+    pos_names = document.getElementsByClassName('pos-name')
+    for (i = 0; i < pos_names.length; i++)
+        if (pos_names[i].style['display'] == 'block') {
+            pos_name = pos_names[i].value
+            break
+        }
+    if (pos_field == '不拘')
+        pos_field = ''
+    if (pos_name == '不拘')
         pos_name = ''
-        pos_names = document.getElementsByClassName('pos-name')
-        for (i = 0; i < pos_names.length; i++)
-            if (pos_names[i].style['display'] == 'block') {
-                pos_name = pos_names[i].value
-                break
-            }
-        if (pos_field == '不拘')
-            pos_field = ''
-        if (pos_name == '不拘')
-            pos_name = ''
 
-        area_cc = document.getElementById('area-cc').value
+    area_cc = document.getElementById('area-cc').value
+    area_td = ''
+    areas = document.getElementsByClassName('area-td')
+    for (i = 0; i < areas.length; i++)
+        if (areas[i].style['display'] == 'block') {
+            area_td = areas[i].value
+            break
+        }
+    if (area_cc == '不拘')
+        area_cc = ''
+    if (area_td == '不拘')
         area_td = ''
-        areas = document.getElementsByClassName('area-td')
-        for (i = 0; i < areas.length; i++)
-            if (areas[i].style['display'] == 'block') {
-                area_td = areas[i].value
-                break
-            }
-        if (area_cc == '不拘')
-            area_cc = ''
-        if (area_td == '不拘')
-            area_td = ''
 
-        job_type = document.getElementById('job-type').value.substr(0, 2)
-        salary = document.getElementById('salary').value
-        if (!salary || !salary.match("^[0-9]+$")) {
-            salary = 'NULL'
-            document.getElementById('salary').value = ''
-        }
-
-        degree = document.getElementById('degree').value
-        exp_year = document.getElementById('exp-year').value
-        night = document.getElementById('night').value
-        worktime = document.getElementById('worktime').value
-        if (!exp_year || !exp_year.match("^[0-9]+$")) {
-            exp_year = '0'
-            document.getElementById('exp-year').value = '0'
-        }
-        if (!worktime || !worktime.match("^[0-9]+$")) {
-            worktime = '0'
-            document.getElementById('worktime').value = ''
-        }
-
-        loader.setAttribute('identifier', iden)
-        loader.style['display'] = 'block'
-        resBlock.style['display'] = 'none'
-        resContainer.innerHTML = ''
-
-        statistics = document.getElementsByClassName('statistic')
-        statistics_loading = document.getElementsByClassName('statistic-loading')
-        for (i = 0; i < statistics.length; i++)
-            statistics[i].style['display'] = 'none'
-        for (i = 0; i < statistics.length; i++)
-            statistics_loading[i].style['display'] = 'block'
-        document.getElementById('salary-avg-text').textContent = ''
-        document.getElementById('salary-max-text').textContent = ''
-        document.getElementById('salary-min-text').textContent = ''
-        document.getElementById('exp-avg-text').textContent = ''
-        document.getElementById('worktime-avg-text').textContent = ''
-        document.getElementById('area-cc-max').textContent = ''
-        document.getElementById('area-cctd-max').textContent = ''
-
-        data = {
-            from: 'advance',
-            search: search,
-            identifier: iden,
-            pos_field: pos_field,
-            pos_name: pos_name,
-            area_cc: area_cc,
-            area_td: area_td,
-            job_type: job_type,
-            salary: salary,
-            degree: degree,
-            exp_year: exp_year,
-            is_night: night,
-            worktime: worktime,
-            time: new Date().getTime()
-        }
-        console.log(data)
-        ws.send(JSON.stringify(data))
+    job_type = document.getElementById('job-type').value.substr(0, 2)
+    salary = document.getElementById('salary').value
+    if (!salary || !salary.match("^[0-9]+$")) {
+        salary = 'NULL'
+        document.getElementById('salary').value = ''
     }
-    else {
-        document.getElementById('search-input').className = 'form-control form-control-lg is-invalid'
+
+    degree = document.getElementById('degree').value
+    exp_year = document.getElementById('exp-year').value
+    night = document.getElementById('night').value
+    worktime = document.getElementById('worktime').value
+    if (!exp_year || !exp_year.match("^[0-9]+$")) {
+        exp_year = '0'
+        document.getElementById('exp-year').value = '0'
     }
+    if (!worktime || !worktime.match("^[0-9]+$")) {
+        worktime = '0'
+        document.getElementById('worktime').value = ''
+    }
+
+    loader.setAttribute('identifier', iden)
+    loader.style['display'] = 'block'
+    resBlock.style['display'] = 'none'
+    resContainer.innerHTML = ''
+
+    statistics = document.getElementsByClassName('statistic')
+    statistics_loading = document.getElementsByClassName('statistic-loading')
+    for (i = 0; i < statistics.length; i++)
+        statistics[i].style['display'] = 'none'
+    for (i = 0; i < statistics.length; i++)
+        statistics_loading[i].style['display'] = 'block'
+    document.getElementById('salary-avg-text').textContent = ''
+    document.getElementById('salary-max-text').textContent = ''
+    document.getElementById('salary-min-text').textContent = ''
+    document.getElementById('exp-avg-text').textContent = ''
+    document.getElementById('worktime-avg-text').textContent = ''
+    document.getElementById('area-cc-max').textContent = ''
+    document.getElementById('area-cctd-max').textContent = ''
+
+    data = {
+        from: 'advance',
+        search: search,
+        identifier: iden,
+        pos_field: pos_field,
+        pos_name: pos_name,
+        area_cc: area_cc,
+        area_td: area_td,
+        job_type: job_type,
+        salary: salary,
+        degree: degree,
+        exp_year: exp_year,
+        is_night: night,
+        worktime: worktime,
+        time: new Date().getTime()
+    }
+    console.log(data)
+    ws.send(JSON.stringify(data))
 }
 
 window.onload = () => {
